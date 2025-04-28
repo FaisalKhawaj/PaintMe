21ec765693d63585080bb849ebcaa7e52933a1ed
FaisalKhawaj <khawajfaisal981@gmail.com>
2025-04-28 15:48:11 +0500

    fix

--- src/screens/enteremail/index.tsx
+++ src/screens/enteremail/index.tsx
@@ -118,7 +118,7 @@ export const EnterEmail = () => {
               </Text>
             </MotiView>
             <MotiView
-              style={{ gap: 10, bottom: !isKeyboardVisible ? 30 : 0 }}
+              style={{ gap: 10, bottom: !isKeyboardVisible ? 20 : 0 }}
               from={{ translateY: 50, opacity: 0 }}
               animate={{ translateY: 0, opacity: 1 }}
               transition={{ delay: 600, duration: 500 }}
